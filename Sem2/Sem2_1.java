9128c5c3e4e98739307d702fd514d1cd9d931d2b
Mariia <maria.gadova@gmail.com>
2023-07-13 20:35:10 +0300

    Sem2_2

--- Sem2/Sem2_1.java
+++ Sem2/Sem2_1.java
@@ -8,18 +8,18 @@ package Java1.Sem2;
 
 public class Sem2_1 {
     public static void main(String[] args) {
-        char c1 = 'a';
+        char c1 = 'a'; // singular quotation marks are only to be used with chars
         char c2 = 'b';
         int n = 6;
         System.out.println(combine(c1, c2, n));
     }
     static String combine(char c1, char c2, int n) {
-        StringBuilder stringbuilder = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i <= n / 2; i ++) {
-            stringbuilder.append(c1);
-            stringbuilder.append(c2);
+            stringBuilder.append(c1);
+            stringBuilder.append(c2);
         }
-        return stringbuilder.toString();
+        return stringBuilder.toString();
     }
     
 }
---
+++ Sem2/Sem2_2.java
@@ -0,0 +1,35 @@
+package Java1.Sem2;
+
+/*
+ * Напишите метод, который сжимает строку.
+ * Пример : aaaabbbcdd
+ * Результат : a4b3cd2
+ */
+
+public class Sem2_2 {
+    public static void main(String[] args) {
+        String s = "aaaabbbcdd";
+        System.out.println(squeeze(s));
+    }
+    static String squeeze(String line) {
+        int count = 1;
+        char[] chars = line.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i < chars.length; i ++) {
+            if (chars[i] == chars[i-1]) {
+                count ++;
+            } else {
+                stringBuilder.append(chars[i-1]);
+                if (count != 1) {
+                    stringBuilder.append(count);
+                    count = 1;
+                }
+            }
+        }
+        stringBuilder.append(chars[chars.length - 1]); //  to include the last letter (d2)
+        if (count != 1) {
+            stringBuilder.append(count);
+        }
+        return stringBuilder.toString();
+    }
+}
