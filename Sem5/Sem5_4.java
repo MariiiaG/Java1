d67fc1c4baabeba37641b8e4e816116b1faaeda9
Mariia <maria.gadova@gmail.com>
2023-07-24 23:37:29 +0300

    Sem5_3_and_4

---
+++ Sem5/Sem5_4.java
@@ -0,0 +1,44 @@
+package Java1.Sem5;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Sem5_4 {
+      /*
+    Взять набор строк, например,
+
+    Мороз и солнце день чудесный
+    Еще ты дремлешь друг прелестный
+    Пора красавица проснись.
+
+    Написать метод, который отсортирует эти строки по длине с помощью TreeMap.
+    Строки с одинаковой длиной не должны “потеряться”.
+     */
+    public static void main(String[] args) {
+        String s = "Мороз и солнце день чудесный\n" +
+                "Еще ты дремлешь друг прелестный\n" +
+                "Пора красавица проснись.";
+        s = s.replace(".", "");
+        s = s.replace("\n", " ");
+        s = s.replace("  ", " ");
+        String[] words = s.split(" ");
+
+        //Map<Integer, List<String>> map = new TreeMap<>(Comparator.reverseOrder()); // will list in reverse
+        Map<Integer, List<String>> map = new TreeMap<>();
+        for (String word: words){
+            int len = word.length();
+            if (map.containsKey(len)){
+                List<String> list = map.get(len);
+                list.add(word);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(word);
+                map.put(len, list);
+            }
+        }
+        System.out.println(map);
+    }
+}
